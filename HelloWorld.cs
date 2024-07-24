31cada689cc41b651a6c01efe53707daad367ffa
Sizor361 <142895529+Sizor361@users.noreply.github.com>
2024-07-24 14:24:50 +0300

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    
+    void Start()
+    {
+        print("Hello World!!!"); //Вывод в консоль любой информации
+    }
+
+    void Update()
+    {
+        
+    }
+}
